9079229728c2a36fbaa72560c5fcb444807d9a4b
Miloš Guba <milos.guba@kosickaakademia.sk>
2020-10-09 07:56:48 +0200

    meno

---
+++ meno.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=100;
+        int count=0;
+        while(i<1000){
+            i++;
+            if(i%10!=(i/10)%10 && (i/10)%10!=(i/100)%10 && i%10!=(i/100)%10){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+        }
